6db1d30479b4dbcd41474f759caa6c0013794dfb
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2025-11-08 21:27:03 +0530

    Time: 6 ms (23.13%) | Memory: 175.4 MB (5.28%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        int[] pos = new int[n];
-        int[] neg = new int[n];
-        int p = 0, ne = 0;
-
-        // Separate positive and negative numbers
-        for (int num : nums) {
-            if (num > 0) pos[p++] = num;
-            else neg[ne++] = num;
-        }
-
         int[] result = new int[n];
         int i = 0, j = 0, k = 0;
 
-        // Alternate positive and negative
-        while (i < p && j < ne) {
-            result[k++] = pos[i++];
-            result[k++] = neg[j++];
+        // First pass: place positives at even indices
+        for (int p = 0; p < n; p++) {
+            if (nums[p] > 0) {
+                result[i] = nums[p];
+                i += 2; // even index
+            }
         }
 
-        // Add remaining positives
-        while (i < p) result[k++] = pos[i++];
-        // Add remaining negatives
-        while (j < ne) result[k++] = neg[j++];
+        // Second pass: place negatives at odd indices
+        for (int ne = 0; ne < n; ne++) {
+            if (nums[ne] < 0) {
+                result[j + 1] = nums[ne];
+                j += 2; // odd index
+            }
+        }
 
         return result;
     }
