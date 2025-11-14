de5dfac068c762de7064f31f9186b06e5abf13f5
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2025-11-14 16:15:09 +0530

    Time: 2 ms (99.81%) | Memory: 52.7 MB (5.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
+        int count1 = 0, count2 = 0;
+        int e1 = Integer.MIN_VALUE;
+        int e2 = Integer.MIN_VALUE;
+
+        for(int i=0; i<=n-1; i++) {
+            if(count1 == 0 && nums[i] != e2) {
+                count1 = 1;
+                e1 = nums[i];
+            } else if(count2 == 0 && nums[i] != e1) {
+                count2 = 1;
+                e2 = nums[i];
+            } else if(e1 == nums[i]) {
+                count1++;
+            } else if(e2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
         List<Integer> res = new ArrayList<>();
+        count1 = 0;
+        count2 = 0;
 
         for(int i=0; i<n; i++) {
-            if(res.size() == 0 || res.get(0) != nums[i]) {
-                int count = 0;
-                for(int j=0; j<=n-1; j++) {
-                    if(nums[j] == nums[i]) {
-                        count++;
-                    }
-                }
-                if(count > n/3) {
-                    res.add(nums[i]);
-                }
+            if(e1 == nums[i]) {
+                count1++;
             }
-            if(res.size() == 2) {
-                break;
+            if(e2 == nums[i]) {
+                count2++;
             }
+        }
+
+        int mini = (n/3) + 1;
+        
+        if(count1 >= mini) {
+            res.add(e1);
+        }
+        if(count2 >= mini) {
+            res.add(e2);
         }
 
         return res;
