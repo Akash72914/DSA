ae62bbb315c7c61e282f38d218e8e45e14e36724
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2025-11-18 22:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (9.74%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,55 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int first = -1;
-        int last = -1;
+    private int lowerBound(int[] nums, int n, int target) {
+        int left = 0;
+        int right = n - 1;
+        int ans = -1;
 
-        for(int i=0; i<n; i++) {
-            if(nums[i] == target) {
-                if(first == -1) {
-                    first = i;
-                }
-                last = i;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } 
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private int upperBound(int[] nums, int n, int target) {
+        int left = 0;
+        int right = n - 1;
+        int ans = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } 
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
 
-        return new int[]{first, last};
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+
+        int lb = lowerBound(nums, n, target);
+        int ub = upperBound(nums, n, target);
+
+        return new int[]{lb, ub};
     }
 }
