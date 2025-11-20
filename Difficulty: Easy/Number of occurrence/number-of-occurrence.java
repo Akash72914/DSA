f5ca54e324fcaaadc87a12947f065d3ae4d426e2
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2025-11-20 11:04:36 +0530

    Refactor countFreq method to use binary search

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -1,14 +1,60 @@
 class Solution {
-    int countFreq(int[] arr, int target) {
-        int n = arr.length;
-        int count = 0;
-        
-        for(int i=0; i<n; i++) {
-            if(arr[i] == target) {
-                count++;
+    private int firstOccurance(int[] nums, int n, int target) {
+        int left = 0;
+        int right = n - 1;
+        int first = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } 
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
             }
         }
+        return first;
+    }
+
+    private int lastOccurance(int[] nums, int n, int target) {
+        int left = 0;
+        int right = n - 1;
+        int last = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } 
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return last;
+    }
+
+    int countFreq(int[] nums, int target) {
+        int n = nums.length;
+
+        int first = firstOccurance(nums, n, target);
+
+        if(first == -1) {
+            return 0;
+        }
+
+        int last = lastOccurance(nums, n, target);
         
-        return count;
+        return last - first + 1;
     }
 }
