3db1b932cb0d1f120531080e7603d46b0eaaecca
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2026-05-28 12:39:49 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (69.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,66 @@
 class Solution {
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        // Always binary search on smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
         int m = nums1.length;
         int n = nums2.length;
 
-        List<Integer> temp = new ArrayList<>();
+        int low = 0;
+        int high = m;
 
-        int i = 0;
-        int j = 0;
+        while (low <= high) {
 
-        while(i < m && j < n) {
+            int cut1 = low + (high - low) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
 
-            if(nums1[i] <= nums2[j]) {
-                temp.add(nums1[i++]);
-            } else {
-                temp.add(nums2[j++]);
+            int left1 = (cut1 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums1[cut1 - 1];
+
+            int left2 = (cut2 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums2[cut2 - 1];
+
+            int right1 = (cut1 == m)
+                    ? Integer.MAX_VALUE
+                    : nums1[cut1];
+
+            int right2 = (cut2 == n)
+                    ? Integer.MAX_VALUE
+                    : nums2[cut2];
+
+            // correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // even length
+                if ((m + n) % 2 == 0) {
+
+                    return (
+                            Math.max(left1, left2)
+                            + Math.min(right1, right2)
+                    ) / 2.0;
+                }
+
+                // odd length
+                return Math.max(left1, left2);
+            }
+
+            // move left
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+
+            // move right
+            else {
+                low = cut1 + 1;
             }
         }
 
-        while(i < m) {
-            temp.add(nums1[i++]);
-        }
-
-        while(j < n) {
-            temp.add(nums2[j++]);
-        }
-
-        int l = temp.size();
-
-        if(l % 2 == 1) {
-            return temp.get(l / 2);
-        }
-
-        return (temp.get(l/2) + temp.get((l/2)-1)) / 2.0;
+        return 0.0;
     }
 }
