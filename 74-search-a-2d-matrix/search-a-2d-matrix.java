17336e92bc1c64a00e32dbddd555b97c743e4da9
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2026-05-29 12:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (60.51%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0;
         int j = matrix[0].length - 1;
 
-        while(i<matrix.length && j>=0) {
+        while(i < matrix.length && j >= 0) {
             if(matrix[i][j] == target) {
                 return true;
             } else if(matrix[i][j] > target) {
