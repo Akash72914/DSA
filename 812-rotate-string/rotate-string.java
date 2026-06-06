3a1c7b34a3428d15feda774ba1fdd47300f7d069
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2026-06-06 11:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.23%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -5,15 +5,6 @@ class Solution {
             return false;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.equals(goal)) {
-                return true;
-            }
-
-            s = s.substring(1) + s.charAt(0);
-        }
-
-        return false;
+        return (s + s).contains(goal);
     }
 }
