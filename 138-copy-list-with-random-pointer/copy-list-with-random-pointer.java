cb4806f1ee8671a057441926725ab2351315f1cc
Akash Yadav <136895185+Akash72914@users.noreply.github.com>
2026-08-17 11:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (73.10%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,38 +1,49 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
-        Node temp = head;
-        HashMap<Node, Node> map = new HashMap<>();
 
-        while(temp != null) {
-            Node newNode = new Node(temp.val);
-            map.put(temp, newNode);
-            temp = temp.next;
+        if (head == null) {
+            return null;
+        }
+
+        Node temp = head;
+
+        while (temp != null) {
+
+            Node copyNode = new Node(temp.val);
+
+            copyNode.next = temp.next;
+            temp.next = copyNode;
+
+            temp = temp.next.next;
         }
 
         temp = head;
 
-        while(temp != null) {
-            Node copyNode = map.get(temp);
-            copyNode.next =map.get(temp.next);
-            copyNode.random = map.get(temp.random);
+        while (temp != null) {
+
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+
+            temp = temp.next.next;
+        }
+
+        temp = head;
+        Node copyHead = head.next;
+
+        while (temp != null) {
+
+            Node copyNode = temp.next;
+
+            temp.next = copyNode.next;
+
+            if (copyNode.next != null) {
+                copyNode.next = copyNode.next.next;
+            }
+
             temp = temp.next;
         }
 
-        return map.get(head);
+        return copyHead;
     }
 }
